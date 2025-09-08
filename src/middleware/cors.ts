ff85fabe0ea4adf6abf79fc8a790b1c70b2a2ea1
miv986 <miros-98@hotmail.es>
2025-09-08 11:02:20 +0200

    fix/vercel url frontend

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -3,7 +3,8 @@ import cors from 'cors';
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'https://kids-playcenter-web-project-72o0m43vp.vercel.app'],
+    'https://kids-playcenter-web-project-72o0m43vp.vercel.app',
+    'https://kids-playcenter-web-project.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
